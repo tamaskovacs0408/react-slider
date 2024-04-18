72416dd00ce3f23c0cb2ca7c784e7adc236b190b
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-04-18 21:41:54 +0200

    Create images file

---
+++ src/Images.ts
@@ -0,0 +1,10 @@
+import img1 from "../assets/image-1.jpg";
+import img2 from "../assets/image-2.jpg";
+import img3 from "../assets/image-3.jpg";
+import img4 from "../assets/image-4.jpg";
+import img5 from "../assets/image-5.jpg";
+
+const IMAGES = [img1, img2, img3, img4, img5];
+
+
+export default IMAGES;
