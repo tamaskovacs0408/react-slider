4304aa2711fdb08dae1cd5f9c1a6be7734671af7
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-04-19 21:38:05 +0200

    import images

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import img1 from '/image-1.jpg';
+import img2 from '/image-2.jpg';
+import img3 from '/image-3.jpg';
+import img4 from '/image-4.jpg';
+import img5 from '/image-5.jpg';
+
+const IMAGES = [img1, img2, img3, img4, img5];
+
 import ImageSliderFirst from "./components/ImageSliderFirst"
+
 
 function App() {
 
   return (
     <>
-      <ImageSliderFirst />
+      <ImageSliderFirst imageUrls={IMAGES} />
     </>
   )
 }
