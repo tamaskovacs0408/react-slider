c17b19c1038c354b3db714c8de5b548674152468
Tamás Kovács <kovacs.tamas0408@gmail.com>
2024-04-21 09:26:23 +0200

    Update imports

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,16 @@ import img5 from '/image-5.jpg';
 
 const IMAGES = [img1, img2, img3, img4, img5];
 
-import ImageSliderFirst from "./components/ImageSliderFirst"
+// import ImageSliderFirst from "./components/ImageSliderFirst"
+import ImageSliderSecond from './components/ImageSliderSecond';
 
 
 function App() {
 
   return (
     <main className="mt-3 max-w-1200 w-full h-500 flex justify-center items-center">
-      <ImageSliderFirst imageUrls={IMAGES} />
+      {/* <ImageSliderFirst imageUrls={IMAGES} /> */}
+      <ImageSliderSecond imageUrls={IMAGES} />
     </main>
   )
 }
